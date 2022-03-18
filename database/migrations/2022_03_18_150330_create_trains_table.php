7c92d02c49e202dd470dcbcecdc21b8e9e98de71
Narresx <92748159+Narresx@users.noreply.github.com>
2022-03-18 16:49:46 +0100

    add trains structure

---
+++ database/migrations/2022_03_18_150330_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 30);
+            $table->string('departure_station', 40);
+            $table->string('arrival_station', 40);
+            $table->datetime('departure_time');
+            $table->datetime('arrival_time');
+            $table->smallInteger('train_code');
+            $table->smallInteger('carreges')->nullable();
+            $table->boolean('on_time')->default(1);
+            $table->boolean('deleted')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
